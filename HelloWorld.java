b9be5dd94fcee73903f3ae2bd7015e4733ae0933
khughes1234 <152438111+khughes1234@users.noreply.github.com>
2023-11-29 14:56:01 -0500

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Print "Hello, World!"
+        System.out.println("Hello, World!");
+
+        // Loop to print numbers 1 through 100
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }
+    }
+}
